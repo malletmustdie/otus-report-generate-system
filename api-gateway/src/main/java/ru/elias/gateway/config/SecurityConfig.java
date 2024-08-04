4e96abce2982b02fcedcc982265f8396fb323aeb
Шокин Илья Сергеевич <isshokin@mts.ru>
2024-08-04 18:01:59 +0300

    feature: add admin endpoints

--- api-gateway/src/main/java/ru/elias/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/ru/elias/gateway/config/SecurityConfig.java
@@ -1,14 +1,25 @@
 package ru.elias.gateway.config;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,10 +40,29 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/api-docs/**",
                                 "/webjars/**").permitAll()
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(configurer -> configurer.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter())))
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        converter.setPrincipalClaimName("preferred_username");
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+            var roles = realmAccess.get("roles");
+            return Stream.concat(authorities.stream(),
+                            roles.stream()
+                                    .filter(role -> role.startsWith("ROLE_"))
+                                    .map(SimpleGrantedAuthority::new)
+                                    .map(GrantedAuthority.class::cast))
+                    .toList();
+        });
+        return converter;
     }
 
     @Bean
--- api-gateway/src/main/java/ru/elias/gateway/service/client/ConfigControlServiceClient.java
+++ api-gateway/src/main/java/ru/elias/gateway/service/client/ConfigControlServiceClient.java
@@ -2,8 +2,11 @@ package ru.elias.gateway.service.client;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PutExchange;
 import ru.elias.gateway.domain.dto.report.Config;
 
 @HttpExchange
@@ -14,5 +17,11 @@ public interface ConfigControlServiceClient {
 
     @GetExchange("/{fileName}")
     Config getConfig(@PathVariable String fileName);
+
+    @PutExchange("/{fileName}")
+    void updateConfig(@PathVariable String fileName, @RequestBody Config config);
+
+    @DeleteExchange("/{fileName}")
+    void deleteConfig(@PathVariable String fileName);
 
 }
